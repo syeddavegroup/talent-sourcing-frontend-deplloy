61c8781b473e662f5f12e5618c5a6222ae46d894
Smiley Ismail <smileyismail67@gmail.com>
2023-02-18 12:06:50 +0530

    added routes on landing page

--- src/components/Landing Page/LandingPage.js
+++ src/components/Landing Page/LandingPage.js
@@ -8,12 +8,23 @@ const LandingPage = () => {
       sx={{
         display: "flex",
         justifyContent: "center",
+        flexDirection: "column",
+        gap: 2,
         alignItems: "center",
         height: "100vh",
       }}
     >
       <Typography variant="h2">
         <Link href="/admin/dashboard">Go to Admin</Link>
+      </Typography>
+      <Typography variant="h2">
+        <Link href="/developer/onBoarding">Go to Developer</Link>
+      </Typography>
+      <Typography variant="h2">
+        <Link href="/vendor/onBoarding">Go to Vendor</Link>
+      </Typography>
+      <Typography variant="h2">
+        <Link href="/client/onBoarding">Go to Client</Link>
       </Typography>
     </Box>
   );
