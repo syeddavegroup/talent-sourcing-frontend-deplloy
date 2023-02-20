324a8bf08733e442aa966cf88873bc9f9c76031f
smileyismail <smileyismail67@gmail.com>
2023-02-20 19:27:16 +0530

    test api added

--- src/components/Users/Admin/AllUsers/AllUsersTable.jsx
+++ src/components/Users/Admin/AllUsers/AllUsersTable.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 import CustomTable from "../../../UI/CustomTable";
 
 import DUMMY_USERS from "../../../../data/dummyUsers";
 
 const AllUsersTable = () => {
+  const [users, setUsers] = useState();
+
   return <CustomTable users={DUMMY_USERS} />;
 };
 
